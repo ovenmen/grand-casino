04b3d578b0a89b9f214436a2105f64d0ac69b0f1
Детев Антон Сергеевич <Detev.A.Se@sberbank.ru>
2019-04-17 11:00:05 +0400

    переименован список мероприятий, добавлен блок с описанием страницы мероприятий

--- pages/events.js
+++ pages/events.js
@@ -4,17 +4,19 @@ import { object } from 'prop-types'
 import fetchDataPage from '../utils/fetch-data-page'
 import HeaderPage from '../components/header-page'
 import Breadcrumbs from '../components/breadcrumbs'
+import Info from '../components/info'
 import AllEvents from '../components/all-events'
 import Review from '../components/review'
 
 const EventsPage = ({ data }) => {
     const { header, headerImage, breadcrumbs } = data
-    const { events, review } = data.content
+    const { info, events, review } = data.content
 
     return (
         <section>
             {breadcrumbs && <Breadcrumbs breadcrumbs={breadcrumbs} />}
             {header && headerImage && <HeaderPage header={header} image={headerImage} />}
+            {info && <Info {...info} />}
             {events && <AllEvents {...events} />}
             {review && <Review {...review} />}
         </section>
