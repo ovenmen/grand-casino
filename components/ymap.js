194105a0cbc0af8a5df0587c07df825a3dae772f
ant <ant@localhost.localdomain>
2019-06-01 21:39:04 +0400

    Добален блок с франшизой

--- components/ymap.js
+++ components/ymap.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import { array } from 'prop-types'
+import React, { useState } from 'react'
+import { array, string } from 'prop-types'
 import { map, uniqueId } from 'lodash'
 import { YMaps, Map, ZoomControl, GeolocationControl, Placemark } from 'react-yandex-maps'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -7,87 +7,85 @@ import { faSpinner } from '@fortawesome/free-solid-svg-icons'
 
 const LABEL = '<strong>GC</strong>'
 
-class YMap extends Component {
-    state = {
-        instance: null
+const YMap = ({ header, items }) => {
+    const [instance, setInstance] = useState(null)
+
+    const handleInstanceRef = (instance) => {
+        setInstance(instance)
     }
 
-    handleInstanceRef = (instance) => {
-        this.setState({
-            instance: instance
-        })
-    }
-
-    render () {
-        const { items } = this.props
-        const { instance } = this.state
-
-        return (
-            <div className="map-container">
-                <YMaps>
-                    {!instance &&
-                        <div className="spinner-container">
-                            <span className="icon fa-2x fa-pulse"><FontAwesomeIcon icon={faSpinner} /></span>
-                            <p className="spinner-text text-center">Карта загружается, подождите...</p>
-                        </div>
-                    }
-                    <Map
-                        width="100%"
-                        height="30rem"
-                        defaultState={{ center: [65, 100], zoom: 3 }}
-                        defaultOptions={{ minZoom: 3 }}
-                        modules={['geoObject.addon.balloon', 'geoObject.addon.hint']}
-                        instanceRef={this.handleInstanceRef}
-                    >
-                        <ZoomControl options={{ size: 'small', zoomDuration: 200 }} />
-                        <GeolocationControl options={{ float: 'left' }} />
-                        {map(items, (item, index) => (
-                            <Placemark
-                                key={uniqueId(index)}
-                                defaultGeometry={[item.lat, item.long]}
-                                defaultProperties={{ iconContent: item.cooperation && LABEL, hintContent: item.city }}
-                                defaultOptions={{ preset: item.cooperation ? 'islands#nightStretchyIcon' : 'islands#nightCircleDotIcon' }}
-                            />
-                        ))}
-                    </Map>
-                </YMaps>
-
-                <style jsx>{`
-                    .map-container {
-                        max-width: 100%;
-                        position: relative;
-                        padding:5vw;
-                        min-height: 30rem;
-                    }
-                    .spinner-container {
-                        position: absolute;
-                        top: 0;
-                        bottom: 0;
-                        left: 0;
-                        right: 0;
-                        height: 30rem;
-                    }
-                    .map-container .icon {
-                        position: absolute;
-                        top: 50%;
-                        left: 50%;
-                        margin-top: -3rem;
-                        margin-left: -1.5rem;
-                    }
-                    .map-container .spinner-text {
-                        position: absolute;
-                        top: 50%;
-                        left: 0;
-                        right: 0;
-                        margin-top: 2rem;
-                    }
-                `}</style>
+    return (
+        <section className="map-container">
+            <div className="grid-x">
+                <div className="cell">
+                    <h2 className="margin-bottom-2 text-center color-white">{header}</h2>
+                </div>
             </div>
-        )
-    }
+            <YMaps>
+                {!instance &&
+                    <div className="spinner-container">
+                        <span className="icon fa-2x fa-pulse"><FontAwesomeIcon icon={faSpinner} /></span>
+                        <p className="spinner-text text-center">Карта загружается, подождите...</p>
+                    </div>
+                }
+                <Map
+                    width="100%"
+                    height="30rem"
+                    defaultState={{ center: [65, 100], zoom: 3 }}
+                    defaultOptions={{ minZoom: 3 }}
+                    modules={['geoObject.addon.balloon', 'geoObject.addon.hint']}
+                    instanceRef={handleInstanceRef}
+                >
+                    <ZoomControl options={{ size: 'small', zoomDuration: 200 }} />
+                    <GeolocationControl options={{ float: 'left' }} />
+                    {map(items, (item, index) => (
+                        <Placemark
+                            key={uniqueId(index)}
+                            defaultGeometry={[item.lat, item.long]}
+                            defaultProperties={{ iconContent: item.cooperation && LABEL, hintContent: item.city }}
+                            defaultOptions={{ preset: item.cooperation ? 'islands#nightStretchyIcon' : 'islands#nightCircleDotIcon' }}
+                        />
+                    ))}
+                </Map>
+            </YMaps>
+
+            <style jsx>{`
+                .map-container {
+                    max-width: 100%;
+                    position: relative;
+                    padding:5vw;
+                    min-height: 30rem;
+                    background: #38286d;
+                }
+                .spinner-container {
+                    position: absolute;
+                    top: 0;
+                    bottom: 0;
+                    left: 0;
+                    right: 0;
+                    height: 30rem;
+                }
+                .map-container .icon {
+                    position: absolute;
+                    top: 50%;
+                    left: 50%;
+                    margin-top: -3rem;
+                    margin-left: -1.5rem;
+                }
+                .map-container .spinner-text {
+                    position: absolute;
+                    top: 50%;
+                    left: 0;
+                    right: 0;
+                    margin-top: 2rem;
+                }
+            `}</style>
+        </section>
+    )
 }
 
 YMap.propTypes = {
+    header: string,
     brand: array,
     items: array
 }
--- pages/franchise.js
+++ pages/franchise.js
@@ -4,17 +4,19 @@ import { object } from 'prop-types'
 import fetchDataPage from '../utils/fetch-data-page'
 import HeaderPage from '../components/header-page'
 import Breadcrumbs from '../components/breadcrumbs'
+import FranchiseInfo from '../components/franchise-info'
 import Ymap from '../components/ymap'
 import Action from '../components/action'
 
 const PricesPage = ({ data }) => {
     const { header, headerImage, breadcrumbs } = data
-    const { action, map } = data.content
+    const { franchise, action, map } = data.content
 
     return (
         <section>
             {breadcrumbs && <Breadcrumbs breadcrumbs={breadcrumbs} />}
             {header && headerImage && <HeaderPage header={header} image={headerImage} />}
+            {franchise && <FranchiseInfo {...franchise} />}
             {map && <Ymap {...map} />}
             {action && <Action {...action} />}
         </section>
