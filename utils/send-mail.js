df328874dbbf12e82f7be41ce97332881e39e75a
Anton Detev <det-anton@yandex.ru>
2019-07-29 00:21:32 +0400

    Обновил картинки

--- utils/send-mail.js
+++ utils/send-mail.js
@@ -1,19 +1,33 @@
 const nodemailer = require('nodemailer')
 
-const host = 'smtp.yandex.ru'
+const host = 'smtp.timeweb.ru'
 const port = 465 // true for 465, false for other ports
 const secure = true
-const user = 'det-anton'
-const pass = 'I71&fyh5'
-const from = '"Anton Detev" <det-anton@yandex.ru>'
+const user = 'admin'
+const pass = 'zd8WxVUUrfjEr9a'
+const from = '"Grand Casino" <admin@grand-casino.com.ru>'
 const to = 'detanton@gmail.com'
 
 const sendMail = async (data) => {
     if (data) {
         try {
             const { subject, html } = data
-            const transporter = await nodemailer.createTransport({ host, port, secure, auth: { user, pass } })
-            const info = await transporter.sendMail({ from, to, subject, html })
+            const transporter = await nodemailer.createTransport({
+                host,
+                port,
+                secure,
+                auth: { user, pass },
+                tls: {
+                    // do not fail on invalid certs
+                    rejectUnauthorized: false
+                }
+            })
+            const info = await transporter.sendMail({
+                from,
+                to,
+                subject,
+                html
+            })
         
             // eslint-disable-next-line no-console
             console.log('Message sent: %s', info.messageId)
